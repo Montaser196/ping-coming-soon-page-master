d33cec388cf9fadde994d75ab9b8968cd1d7f336
montaser-dev <abdallahelmontaser786@gmail.com>
2021-12-14 14:30:50 +0100

    Finishe dthe pring coming soon page

---
+++ condition.js
@@ -0,0 +1,20 @@
+function validation() {
+  // select elements
+    let email = document.getElementById("email_addr");
+    let text = document.getElementById("text");
+    // define regexp
+    let pattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    // check if the email field match the regexp
+      if(email.value.match(pattern)){
+        email.classList.remove("invalid");
+        text.innerHTML= ""
+      }
+      else{
+        email.classList.add("invalid");
+        text.innerHTML= "Please provide a valid email adress"
+      }
+}
+
+document.getElementById("email_addr").addEventListener("keyup",function(){
+  validation()
+})
